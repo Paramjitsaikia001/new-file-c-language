8d69d64ac4e1d97eec0e7055f4b67514a748223c
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2023-09-09 11:05:53 +0530

    edit

--- paramjit.c
+++ paramjit.c
@@ -5,10 +5,46 @@
  	return 1;
  	else return(a*factorial(a-1));
  }
+ int isprime(int a){
+ 		int i;
+ 	for(i=2;i<a;i++)
+ 	if(a%i==0)
+ 	return(0);
+ 	return(1);
+ }
+ int isamongs(int a){
+ 	int n=a,sum=0,deno;
+ 	while(a!=0){
+ 		deno=n%10;
+ 		sum=sum+deno*deno*deno;
+ 		n/=10;
+ 		
+	 }
+	 if(sum==a)
+	 return (1);
+	 else
+	 return (0);
+ }
+ 
  int main(){
- 	int a,f;
+ 	int i, a,f,p,m;
  	printf("enter a number");
  	scanf("%d",&a);
  	f=factorial( a);
- 	printf("%d",f);
+ 	printf("%d\n",f);
+ 	if (isprime(a)) {
+        printf("%d is a prime number.\n", a);
+    } else {
+        printf("%d is not a prime number.\n", a);
+    }
+ 	if(isamongs(a)){
+ 	 printf("%d is an Armstrong number.\n", a);
+    } else {
+        printf("%d is not an Armstrong number.\n", a);
+    }	
+	
+	 
+ 
+
+
  }
