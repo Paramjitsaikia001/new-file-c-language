14196464e054e450b6b450675a90a0926ec6ef58
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2023-09-16 16:26:15 +0530

    update 0.3

--- newfuction.c
+++ newfuction.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+//to check odd or even
  oddoreven(int a){
 		if(a%2==0)
 		return 1;
 		else 
 		return 0;
 	}
+
+    //to find the reverse
 int revrse(int a){
 	int sum=0;
 	while(a!=0){
@@ -14,8 +17,16 @@ int revrse(int a){
 	}
 	return sum;
 }
+void swap(int *a,int *b){
+	int t;
+	*a=*a ^ *b;
+    *b=*a^*b;
+	*a=*a^*b;
+}
 	int main()
 	{
+
+        //to check odd or even
 		int y;
 	 printf("enter y for check odd or even..");
 	 scanf("%d",&y);
@@ -27,10 +38,20 @@ int revrse(int a){
 		printf("\n%d is odd\n",y);
 	 }
 
+ //to find the reverse
 int z;
 printf("enter z to reverse...");
 scanf("%d",&z);
 
 printf("\n%dis the reverse of %d\n ",revrse(z),z);
 return 0;
+
+// swap two number
+	int a,b;
+	printf("enter two number");
+	scanf("%d\n%d",&a,&b);
+	swap(&a,&b);
+	printf("a=%d b=%d",a,b);
 	}
+
+	
--- paramjit.c
+++ paramjit.c
@@ -6,7 +6,7 @@
  	// else 
 	// return(a*factorial(a-1));
 	int i,f=1;
-	for(i=a;i<=a,i!=0;i--)
+	for(i=a;i!=0;i--)
 	f*=i;
 	return(f);
   }
@@ -14,7 +14,7 @@
   //for prime number
  int isprime(int a){
  		int i;
- 	for(i=2;i<a;i++)
+ 	for(i=2;i<=a/2;i++)
  	if(a%i==0)
  	return(0);
  	return(1);
@@ -22,6 +22,11 @@
  
  //print prime number
  void printprime(int a,int b){
+	if(a>b){
+		int t=a;
+		a=b;
+		b=t;
+	}
 	int i;
 	printf("\nthe prime are");
 	for(i=a;i<=b;i++){
@@ -46,6 +51,11 @@
 
 //print armstrong number
 void printarms(int a,int b){
+	if(a>b){
+		int t=a;
+		a=b;
+		b=t;
+	}
 	int i;
 	printf("the armstrongs number");
 	for(i=a;i<=b;i++){
@@ -80,17 +90,23 @@ void printarms(int a,int b){
  }
 	//print perfect number
 	void printperfect(int a,int b){
+		if(a>b){
+		int t=a;
+		a=b;
+		b=t;
+	}
 		int j;
 		printf("\nthe perfect number:");
 		for(j=a;j<=b;j++){
 			if(perfect(j))
-			printf("%d",j);
+			printf(" %d ",j);
 		}
 	}
 
  //for print factor of a number
  void factor(int a)
  {
+	
 	int i;
 	printf("\nthe factors are:");
 	for(i=1;i<=a;i++){
@@ -99,13 +115,40 @@ void printarms(int a,int b){
 		}
 	}
  }
+
+ //swap two number
+ void swap(int *a,int *b){
+	int t;
+	*a=*a ^ *b;
+    *b=*a^*b;
+	*a=*a^*b;
+}
+
+
  int main(){
- 	int  a,f,l;
+	
+ 	int  i,a,f,l;
+	printf("NOTE: enter 1 for find factorial of a number\n");
+	printf("      enter 2 for check prime number or not\n");
+	printf("      enter 3 for check armstrong number or not\n");
+	printf("      enter 4 for find largest of three number\n");
+	printf("      enter 5 for check perfect nummber or not\n");
+	printf("      enter 6 for print prime number\n");
+	printf("      enter 7 for print factor of a number\n");
+	printf("      enter 8 for print armstrong number\n");
+	printf("      enter 9 for print perfect number\n");
+	printf("      enter 10 for swap two number\n\n\n");
+	printf("enter the value of i=");
+	scanf("%d",&i);
+	switch (i){
+
+	case 1:
  	printf("enter a to find factorial....");
  	scanf("%d",&a);
  	f=factorial( a);
  	printf("%d is the factorial of %d\n\n",f,a);
-
+    break;
+	case 2:
 	//for prime number
 	int p;
 	printf("enter p for check it prime or not...");
@@ -115,7 +158,8 @@ void printarms(int a,int b){
     } else {
         printf("%d is not a prime number.\n\n", p);
     }
-	
+	break;
+	case 3:
 	//for armstrongs number
 	int m;
 	printf("enter m for check armstrong or not..");
@@ -125,14 +169,16 @@ void printarms(int a,int b){
     } else {
         printf("%d is not an Armstrong number.\n\n", m);
     }
-
+     break;
+	 case 4:
 	//for largest of three number	
-	int b,c;
-	printf("enter two  number");
-	scanf("%d\n%d",&b,&c);
+	int b,c,d;
+	printf("enter three  number");
+	scanf("%d\n%d\n%d",&b,&c,&d);
      l=largest(largest(a,b),c);
-	 printf("%d is the largest number of %d,%dand%d\n\n",l,a,b,c);
-
+	 printf("%d is the largest number of %d,%dand%d\n\n",l,b,c,d);
+ break;
+ case 5:
 	 //for perfect number
 	 int x;
 	 printf("enter x for check perfect  number or not=");
@@ -141,7 +187,8 @@ void printarms(int a,int b){
       printf("%d is the perfect no\n",x);
      else
      printf("%d is not perfect no\n\n",x);
-	 
+	 break;
+	 case 6:
 	 //print prime number
 	 int start , end;
 	 printf("enter the first number for print prime number");
@@ -149,13 +196,15 @@ void printarms(int a,int b){
 	 printf("enter the last number for print prime number");
 	 scanf("%d",&end);
 	 printprime(start,end);
-	 
-	 //print factor a prime number
+	 break;
+	 case 7:
+	 //print factor a  number
 	 int num;
 	 printf("\n\nenter a  number for find factor...");
 	 scanf("%d",&num);
 	 factor(num);
-
+	 break;
+case 8:
 	 //print armstong
 	int from,to;
 	printf("\n\narmstrong number from=");
@@ -163,7 +212,8 @@ void printarms(int a,int b){
 	printf("to=");
 	scanf("%d",&to);
      printarms(from,to);
-
+break;
+case 9:
 	 //print perfect number
 	 int first,last;
 	 printf("\n\nfirst no for print perfect number=");
@@ -171,4 +221,19 @@ void printarms(int a,int b){
 	 printf("last no for print perfect number=");
 	 scanf("%d",&last);
 	 printperfect(first,last);
+	  break;
+
+	  //swap two number
+	  case 10:
+	  int num1,num2;
+	printf("first number=");
+	scanf("%d",&num1);
+    printf("second number=");
+	scanf("%d",&num2);
+	swap(&num1,&num2);
+	printf("after swap : first number=%d second number=%d",num1,num2);
+	break;
+}
  }
+
+ 
