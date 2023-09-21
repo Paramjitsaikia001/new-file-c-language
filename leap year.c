5b8b8a2ca5b88db80cb8b348c30aaa77bace28b0
Paramjitsaikia001 <143919282+Paramjitsaikia001@users.noreply.github.com>
2023-09-21 21:21:29 +0530

    Update leap year.c

--- leap year.c
+++ leap year.c
@@ -1,1 +1,11 @@
-uhnclud
+#include<stdio.h>
+int main(){
+int year;
+printf("enter a year=");
+scanf("%d",&year);
+if(year %400==0)
+printf("it is a leap year");
+else 
+printf("it is not leap year");
+return 1;
+}
