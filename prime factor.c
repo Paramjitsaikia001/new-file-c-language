993e6c183360b6fc6976ee8984c6951292ddd430
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2023-09-29 09:28:35 +0530

    updated

--- prime factor.c
+++ prime factor.c
@@ -24,10 +24,31 @@ void primefactor(int a){
 	
 	}
 
+	//by recursive
+	void primefactors(int n,int i){
+	if(i==2 && i%2==0)
+	printf("2");
+	 if(n%i==0){
+	 if(prime(i))		
+    printf(" %d ",i);
+}
+	primefactors(n,1+i);
+
+}
+
 int main(){
 	int a;
 	printf("enter a number");
 	scanf("%d",&a);
 	primefactor(a);
-	
+
+
+//by recursive
+int n;
+	printf("enter a number");
+	scanf("%d",&n);
+	primefactors(n,2);
 }
+
+
+
