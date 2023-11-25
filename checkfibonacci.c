d9e03e9a080b872800937ef70c3797daacb7a805
Paramjit Saikia <143919282+Paramjitsaikia001@users.noreply.github.com>
2023-11-25 22:49:27 +0530

    Create checkfibonacci.c

---
+++ checkfibonacci.c
@@ -0,0 +1,19 @@
+45.	//Enter a number and check whethe! it is fihonacci number or not.
+#include<stdio.h>
+int main()
+{
+	int n;
+	int a=0,b=1,c=0;
+	printf("enter the number ");
+	scanf("%d",&n);
+	while(c<n){
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	if(c==n)
+	printf("fibonacci");
+	else
+	printf("not fibonacci");
+	return 0;
+}
