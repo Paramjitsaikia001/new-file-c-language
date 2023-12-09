e6e334d8fb64cc66efbbc4f2675c80846c13c625
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2023-12-09 22:10:07 +0530

    update

---
+++ maxminarray.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the size of the array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the element of the array:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int j,k;
+    for(j=0;j<n;j++)
+    {
+        for(k=0;k<n;k++)
+        {
+            for(int l=k+1;l<n;l++)
+            {
+                if(arr[k]<arr[l])
+                {
+                    arr[k]=arr[l];
+                }
+            }
+        }
+        
+    }
+    printf("max:%d\n",arr[0]);
+    printf("min:%d",arr[j]);
+}
---
+++ vowelarray.c
@@ -0,0 +1,39 @@
+//Print the evenvalued elements
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the size of the array:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter the element of the array:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int j,k;
+    printf("the evenvlue of array:");
+    for(j=0;j<n;j++)
+    {
+         for(k=j+1;k<n;k++)
+        {
+            if (arr[j]==arr[k])
+            {
+            for(int l=k;l<n-1;l++)
+        {
+            arr[l]=arr[l+1];
+        }
+            }
+            
+
+        }
+    
+        if(arr[j]%2==0)
+        {
+            printf("%d",arr[j]);
+        }
+    }
+    return 1;
+
+}
