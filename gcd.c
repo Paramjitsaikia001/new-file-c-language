6c603d3f5db6d89158ca9ed50b1f10f849d7535a
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2023-12-10 12:00:40 +0530

    update

--- gcd.c
+++ gcd.c
@@ -1,5 +1,48 @@
 #include<stdio.h>
 int gcd(int a,int b)
 {
-    int 
+    if(a<b)
+    {
+        int t =a;
+        a=b;
+        b=t;
+    }
+    if(a%b==0)
+    return b;
+    else
+    return gcd(a%b,b);
+}
+int main()
+{
+    int i,a,b;
+    printf("enter two number:");
+    scanf("%d%d",&a,&b);
+    printf("enter the value of i:");
+    scanf("%d",&i);
+    switch(i)
+    {
+
+       case 1:  printf("%d",gcd(a,b));
+       break;
+
+       case 2: 
+       if(a<b)
+       {
+        int t=a;
+        a=b;
+        b=t;
+       }
+       int m=1;
+       int g;
+        while (b != 0) {
+                int temp = b;
+                b = a % b;
+                a = temp;
+        }
+        printf("%d",a);
+        break;
+
+        
+    }
+ return 1;
 }
---
+++ swapmacro.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define SWAP(x,y) {int t=x;x=y;y=t;}
+int main()
+{
+    int a,b;
+    printf("enter two number:");
+    scanf("%d%d",&a,&b);
+    SWAP(a,b)
+    printf("a=%d\nb=%d",a,b);
+
+    return 1;
+
+}
