342d35b11f85f103c5159001bb0428d7dfa20f88
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2024-01-23 23:00:52 +0530

    update

---
+++ c4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int flag =0;
+    int str1[100],str2[100];
+    printf("enter the first string:");
+    scanf("%s",str1);
+    printf("enter the second string:");
+    scanf("%s",str2);
+    for(int i=0;str1[i]!='\0' && str2[i]!='\0';i++)
+    {
+        if(str1[i]!=str2[i])
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==0)
+    printf("strings are equal");
+    else
+    printf("string are not equal");
+    return 0;
+}
